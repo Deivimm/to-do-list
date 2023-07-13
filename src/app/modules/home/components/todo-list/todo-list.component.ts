40731586d377659c420753677ae70f1aa1cb3d9e
Deivid <deividtlima@gmail.com>
2023-07-13 20:10:46 -0300

    feat: 🎸 configuracao do delete

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -9,7 +9,25 @@ import { Tasklist } from '../../model/tasklist';
 })
 export class TodoListComponent {
   public taskList: Array<Tasklist> = [
-      // { task: "Minha nova Task", checked: true },
-      // { task: "Minha nova Task 2", checked: false }
+      { task: "Minha nova Task", checked: true },
+      { task: "Minha nova Task 2", checked: false }
   ] 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1)
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Deseja realmente excluir todas as tarefas?")
+
+    if (confirm){
+      this.taskList = []
+    }
+  }
+
 }
